92be2ce9fbcb590f8dd7119ef5c1ea07b270b8f5
RauthIsaac <rauthishac@gmail.com>
2025-07-16 06:48:49 +0300

    Edit the structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
-import { Home } from './Pages/blank-layout/home/home';
-import { About } from './Pages/blank-layout/about/about';
+import { Home } from './features/Home/home/home';
 import { NotFound } from './Pages/not-found/not-found';
-import { Contact } from './Pages/blank-layout/contact/contact';
+
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component:Home},
-    {path: 'about', component:About},
-    {path: 'contact', component:Contact},
     // {path: '', component:},
 
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./shared/navbar/navbar";
-import { Home } from "./Pages/blank-layout/home/home";
-import { About } from "./Pages/blank-layout/about/about";
-import { Contact } from "./Pages/blank-layout/contact/contact";
+import { Home } from "./features/Home/home/home";
 import { Footer } from "./shared/footer/footer";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Home, About, Contact, Footer],
+  imports: [RouterOutlet, Navbar, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
