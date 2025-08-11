da39cd0bccc09e70d6825866fe268e51cfe821c8
Samaaaaaa <sa2606@fayoum.edu.eg>
2025-08-11 08:53:25 +0300

    resolve conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,12 +10,10 @@ import { AddJob } from './features/profiles/employer/add-job/add-job';
 import { Navbar } from './shared/components/navbar/navbar';
 import { Login } from './auth/login/login';
 import { Signup } from './auth/signup/signup';
-import { SeekerProfileComponent } from './features/profiles/seeker/seeker-profile/seeker-profile';
 import { ConfirmEmail } from './auth/confirm-email/confirm-email';
 import { ForgetPassword } from './auth/forget-password/forget-password';
 import { ResetPassword } from './auth/reset-password/reset-password';
-import { MainLayout } from './layouts/main-layout';
-import { AuthLayout } from './layouts/auth-layout';
+
 import { SeekerProfile } from './features/profiles/seeker/seeker-profile/seeker-profile';
 // import { VerifyEmailComponent } from './verify-email/verify-email';
 
@@ -45,17 +43,4 @@ export const routes: Routes = [
 
     {path: '**', component: NotFound}
     
-];
-
-    {path: '', component: MainLayout, children: [
-        {path: '', redirectTo: 'home', pathMatch: 'full'},
-        {path: 'home', component:Home},
-        { path: 'profile', component: SeekerProfile },
-    ]},
-    {path: '', component: AuthLayout, children: [
-        {path: 'register', component: Signup },
-        {path: 'login', component: Login },
-        // { path: 'verify-email', component: VerifyEmailComponent},
-    ]}
-
 ];
