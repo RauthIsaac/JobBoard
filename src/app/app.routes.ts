ff39d7596d553212ee04217a93864a291853ed4e
Samaaaaaa <sa2606@fayoum.edu.eg>
2025-08-11 08:50:17 +0300

    seeker responsive profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Login } from './auth/login/login';
 import { Signup } from './auth/signup/signup';
 import { MainLayout } from './layouts/main-layout';
 import { AuthLayout } from './layouts/auth-layout';
-import { SeekerProfileComponent } from './features/profiles/seeker/seeker-profile/seeker-profile';
+import { SeekerProfile } from './features/profiles/seeker/seeker-profile/seeker-profile';
 // import { VerifyEmailComponent } from './verify-email/verify-email';
 
 
@@ -13,9 +13,7 @@ export const routes: Routes = [
     {path: '', component: MainLayout, children: [
         {path: '', redirectTo: 'home', pathMatch: 'full'},
         {path: 'home', component:Home},
-        { path: 'profile', component: SeekerProfileComponent },
-        
-
+        { path: 'profile', component: SeekerProfile },
     ]},
     {path: '', component: AuthLayout, children: [
         {path: 'register', component: Signup },
